fcb7152c22b72e7860085e57b0a4360aa5dd649c
AndrewS <andrews@AndrewS.local>
2020-06-27 18:48:11 -1000

    Update codepen url

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -13,7 +13,7 @@ const AboutSection = () => {
         </div>
         <div className="about-section__copy">
           <h2>About</h2>
-          <p>A little about myself. I am a self-taught web developer who discovered the wonderful world of coding in my late twenties. One of my greatest passions in life is listening to challenging digital problems and working out solutions to solve them. I have had the honor to work with many great designers and developers. I live in Honolulu, Hawaii with my beautiful wife Ally and our loyal companion Orion.</p>
+          <p>A little about myself. I am a self-taught web developer who discovered the wonderful world of coding in my late twenties. One of my greatest passions in life is listening to challenging digital problems and working out solutions to solve them. I have had the honor to work with many great designers and developersit. I live in Honolulu, Hawaii with my beautiful wife Ally and our loyal companion Orion.</p>
         </div>
       </div>
     </section>
--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -12,7 +12,7 @@ import './SocialMedia.scss';
 
 const SocialMedia = ({className}) => (
   <nav className={`social-media ${className ? className : ''}`}>
-    <a className="social-media__link social-media__link--code" href="https://codepen.io/targrayen" role="button" aria-label="Codepen"><Codepen /></a>
+    <a className="social-media__link social-media__link--code" href="https://codepen.io/anddaz" role="button" aria-label="Codepen"><Codepen /></a>
     <a className="social-media__link social-media__link--git" href="https://github.com/asipple1" role="button" aria-label="Github"><GitHub /></a>
     <a className="social-media__link social-media__link--linkedin" href="https://www.linkedin.com/in/andy-sipple-7ab92a28/" role="button" aria-label="Linkedin"><Linkedin /></a>
     <a className="social-media__link social-media__link--insta" href="https://www.instagram.com/and_a_z/" role="button" aria-label="instagram"><Instagram /></a>
